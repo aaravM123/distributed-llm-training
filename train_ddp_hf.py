62c4bbdcaba44a514d73b645fa833789dbde181f
aaravM123 <aaravmehra616@gmail.com>
2025-10-02 10:03:04 -0700

    Fix train_ddp_hf.py - remove unused DistributedSampler
    - Removed unused DistributedSampler import and variable
    - Fixed DataLoader to use shuffle=True instead of undefined sampler
    - Simplified script for basic HuggingFace dataset testing

--- train_ddp_hf.py
+++ train_ddp_hf.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import DataLoader, DistributedSampler
+from torch.utils.data import DataLoader
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
@@ -14,8 +14,7 @@ def main():
     dataset = dataset.map(tokenize, batched=True)
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
 
-    train_sampler = DistributedSampler(dataset["train"])
-    train_loader  = DataLoader(dataset["train"], sampler=train_sampler, batch_size=16)
+    train_loader  = DataLoader(dataset["train"], batch_size=16, shuffle=True)
 
     for batch in train_loader:
         print(batch["input_ids"].shape, batch["attention_mask"].shape, batch["label"].shape)
