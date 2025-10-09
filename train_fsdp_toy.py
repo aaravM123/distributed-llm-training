0c5eef71e6c8b712ce1bc7a387cf2c31e0655a8c
aaravM123 <aaravmehra616@gmail.com>
2025-10-09 10:20:52 -0700

    Update

--- train_fsdp_toy.py
+++ train_fsdp_toy.py
@@ -52,7 +52,8 @@ for epoch in range(args.epochs):
         if step % 5 == 0 and dist.get_rank() == 0:
             print(f"Epoch {epoch}, Step {step}, Loss: {loss.item():.4f}")
 
-if dist.get_rank() == 0:
+if dist.is_initialized() and dist.get_rank() == 0:
     print("Peak Memory Usage: ", torch.cuda.max_memory_allocated() / 1e6, "MB")
 
-dist.destroy_process_group()
+if dist.is_initialized():
+    dist.destroy_process_group()
